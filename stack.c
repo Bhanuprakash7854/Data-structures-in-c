f0c3f7234588dea73e509966e123faeb4a7618ae
Bhanuprakash7854 <138193744+Bhanuprakash7854@users.noreply.github.com>
2023-10-11 19:45:49 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,73 @@
+//stack implementation using array in c
+//written by : Bhanu prakash
+
+
+#include<stdio.h>
+//here i am taking stack size as 5
+#define MAX 5
+//defining stack array as global so it can be acessed in all functions
+int stack[MAX],top = -1;
+
+
+void main(){
+	//runnnig an infinite loop
+	while(1)
+	{
+		int choice;
+		printf("Enter the operation :\n");
+		printf("1.push\n2.pop\n3.peek\n4.disiplay\n5.exit\n");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:push();
+			break;
+			case 2:pop();
+			break;
+			case 3:peek();
+			break;
+			case 4:disiplay();
+			break;
+			case 5:return;
+		}
+	}
+}
+
+//to insert an element in stack
+void push()
+{
+	//if stack is full the program will exit
+	if(top==MAX-1)
+	return;
+	
+	int n;
+	printf("Enter the number:");
+	scanf("%d",&n);
+	top++;
+	stack[top]=n;
+}
+
+
+//to remove an element in stack
+void pop()
+{
+	//if stack is empty and you try to pop the program will exit
+	if(top<0)
+	return;
+	top--;
+}
+
+//to print the last inserted value in stack
+void peek()
+{
+	printf("top element is %d",stack[top]);
+}
+
+
+//to disiplay all the values in stack
+void disiplay()
+{
+	for(int i=top;i>0;i--)
+	{
+		printf("%d\t",stack[i]);
+	}
+}
