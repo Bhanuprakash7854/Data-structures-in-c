128b05395824a55ed47299f7abab1d7ba2a6041a
Bhanuprakash7854 <138193744+Bhanuprakash7854@users.noreply.github.com>
2023-10-15 23:26:55 +0530

    Add files via upload

---
+++ circular queue.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 2
+
+void insert();
+void Delete();
+void display();
+int queue[MAX];
+int front =0,rear=-1;
+
+void main(){
+	
+	while(1)
+	{
+		int choice;
+		printf("Enter your choice:\n");
+		printf("1.insert\n2.Delete\n3.display\n4.exit\n");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:insert();
+			break;
+			case 2:Delete();
+			break;
+			case 3:display();
+			break;
+			case 4:exit(0);
+			break;
+		}
+	}
+}
+
+void insert()
+{
+	
+	if((front == 0 && rear == MAX -1) || (front !=0 && rear != -1) && front == rear +1)
+	{
+		printf("queue is full");
+		exit(0);
+	}
+	else
+	{
+		
+		
+		rear++;
+		if(rear == MAX)
+		{
+			rear = (rear)%MAX;
+		}
+		int n;
+		printf("Enter number to insert:");
+		scanf("%d",&n);
+		queue[rear] = n;
+	}
+}
+
+void Delete()
+{
+	if(front == 0 && rear ==-1)
+	{
+		printf("queue is empty");
+		exit(0);
+	}
+	else
+	{
+		front++;
+	}
+}
+
+
+void display()
+{
+	int i;
+	for(i=front;i!=rear;i++)
+	{
+		if(i == MAX){
+			i = (i)%MAX;
+			if(i==rear)
+			break;
+		}
+		printf("%d\t",queue[i]);
+	}
+	printf("%d\n",queue[i]);
+}
